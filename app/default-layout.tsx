cff4de0e9a62866c1fea05c1c59f2a1ba075eb23
tjbagley <tjbagley@gmail.com>
2026-02-19 10:26:52 +1100

    mobile styling

--- app/default-layout.tsx
+++ app/default-layout.tsx
@@ -6,6 +6,11 @@ import { selectAllWordsAndPhrases, selectRecentlyLearntWordsAndPhrases } from ".
 import { selectAllWordLists } from "./store/slices/word-lists.slice";
 import { ExportHelper } from "./helpers/export.helper";
 
+import bookIcon from "./assets/book-outline.svg";
+import listIcon from "./assets/list-outline.svg";
+import categoriesIcon from "./assets/albums-outline.svg";
+import learnIcon from "./assets/star-outline.svg";
+
 function DefaultLayout() {
   const categories = useSelector((state: RootState) => selectAllCategories(state));
   const words = useSelector((state: RootState) => selectAllWordsAndPhrases(state));
@@ -17,20 +22,43 @@ function DefaultLayout() {
     ExportHelper.export(words, categories, lists, recentlyLearntWordsAndPhrases, whatToLearnListId);
   };
 
+  const renderNav = (isFooter = false): React.ReactNode => {
+    return (
+      <nav>
+        <NavLink to="/words">
+          <img src={bookIcon} alt="Words Icon" aria-hidden="true" />
+          <span>{isFooter ? 'Words' : 'Words and Phrases'}</span>
+        </NavLink>
+        <NavLink to="/lists">
+          <img src={listIcon} alt="Lists Icon" aria-hidden="true" />
+          <span>Lists</span>
+        </NavLink>
+        <NavLink to="/categories">
+          <img src={categoriesIcon} alt="Categories Icon" aria-hidden="true" />
+          <span>Categories</span>
+        </NavLink>
+        <NavLink to="/learn">
+          <img src={learnIcon} alt="Learn Icon" aria-hidden="true" />
+          <span>Learn</span>
+        </NavLink>
+        <button onClick={handleExport} type="button">Export</button>
+      </nav>
+    );
+  }
+
   return (
     <div>
       <header>
         <nav>
-            <NavLink to="/words">Words and Phrases</NavLink>
-            <NavLink to="/lists">Lists</NavLink>
-            <NavLink to="/categories">Categories</NavLink>
-            <NavLink to="/learn">Learn</NavLink>
-            <button onClick={handleExport} type="button">Export</button>
+          {renderNav()}
         </nav>
       </header>
       <main>
         <Outlet />
       </main>
+      <footer>
+        {renderNav(true)}
+      </footer>
     </div>
   );
 }
